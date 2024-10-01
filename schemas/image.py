ef8c10572fbef04102d73b4a34c10951e169b633
skylertong <skyler@transcengram.com>
2024-10-01 18:18:01 +0800

    chore: 🚀 增加image基类

--- schemas/image.py
+++ schemas/image.py
@@ -3,14 +3,15 @@ from pydantic import Field
 from schemas.base import BaseSchema
 
 
-class HttpResizeImage(BaseSchema):
+class HttpBaseImage(BaseSchema):
     file_name: str = Field(..., description="文件名")
     file_url: str = Field(..., description="图片地址")
+
+
+class HttpResizeImage(HttpBaseImage):
     width: int | None = Field(None, description="图片宽度", gt=0)  # 宽度可以为 None
     height: int | None = Field(None, description="图片高度", gt=0)  # 高度可以为 None
 
 
 class HttpConvertImage(BaseSchema):
-    file_name: str = Field(..., description="文件名")
-    file_url: str = Field(..., description="图片地址")
     final_type: str = Field(..., description="需要转换的类型")
